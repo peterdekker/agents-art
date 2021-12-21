4b72d95cda5cacd8a59b221c7e4d031f84af7911
Peter Dekker <peter@peterdekker.eu>
2021-12-21 21:47:24 +0100

    Start of loading data

---
+++ art.py
@@ -0,0 +1,36 @@
+import numpy as np
+from neupy import algorithms
+import io
+
+
+def load_vectors(fname):
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+    data = {}
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        data[tokens[0]] = map(float, tokens[1:])
+        print(list(data[tokens[0]]))
+    return data
+
+def main():
+    vectors = load_vectors("binvectors256.vec")
+    data = np.array([
+        [0, 1, 0],
+        [1, 0, 0],
+        [1, 1, 0],
+        [0,1,1],
+        [1,0,1],
+        [1,1,1]
+    ])
+    artnet = algorithms.ART1(
+        step=0.1,
+        rho=0.7,
+        n_clusters=2,
+        shuffle_data=True
+    )
+    prediction = artnet.predict(data)
+    print(prediction)
+
+if __name__ == "__main__":
+    main()
