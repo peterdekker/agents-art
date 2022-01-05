0fda725ff7ab75dab32cd7bab41519cb3b7fc92a
Peter Dekker <peter@peterdekker.eu>
2022-01-05 18:59:42 +0100

    Added evaluation of different parm settings

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ import numpy as np
 import io
 import time
 
+np.random.seed(11)
 
 def map_int(string_list):
     return [int(s) for s in string_list]
@@ -9,28 +10,30 @@ def map_int(string_list):
 def flatten(t):
     return [item for sublist in t for item in sublist]
 
-def load_vectors(fname, n_words):
+def load_vectors(fname, read_first_words=10000, sample_words=1000):
     # t0 = time.time()
     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
     n, dim = map(int, fin.readline().split())
     file_data = []
     words = []
+    sample_indices = np.random.choice(np.arange(read_first_words), sample_words, replace=False)
     for i,line in enumerate(fin):
-        if i >= n_words:
+        if i >= read_first_words:
             break
-        tokens = line.rstrip().split(' ')
-        word = tokens[0]
-        words.append(word)
-        bin_values = [f"{int(decimal_value):064b}" for decimal_value in tokens[1:]]
-        file_data.append(bin_values)
+        if i in sample_indices: # Maybe inefficient if sample_words is high
+            tokens = line.rstrip().split(' ')
+            word = tokens[0]
+            words.append(word)
+            bin_values = [f"{int(decimal_value):064b}" for decimal_value in tokens[1:]]
+            file_data.append(bin_values)
     data_flatten = "".join(flatten(file_data))
     data_int = map_int(list(data_flatten))
-    assert len(data_int)== dim*n_words
-    data_array = np.array(data_int).reshape(n_words, dim)
+    assert len(data_int)== dim*sample_words
+    data_array = np.array(data_int).reshape(sample_words, dim)
     # Split array into n_words. This also joins the groups of 4 longs that represent one vector together
     # t1 = time.time()
     # print(t1-t0)
-    return data_array, words
+    return data_array, words, dim
 
 # def load_vectors(fname):
 #     t0 = time.time()
--- plot.py
+++ plot.py
@@ -1,27 +1,28 @@
-from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+
+from conf import LABEL_DENSITY
 
 plt.rcParams['figure.figsize'] = [10, 6]
 plt.rcParams['figure.dpi'] = 200
 plt.rcParams['savefig.format'] = "pdf"
 
 
-def plot(vectors, labels, clusters, file_label):
+
+def plot(data_standardized, labels, clusters, file_label):
     n_labels = len(labels)
-    assert n_labels == len(clusters) == vectors.shape[0]
-    data_standardized = StandardScaler().fit_transform(vectors)
+    assert n_labels == len(clusters) == data_standardized.shape[0]
     pca = PCA(n_components=2)
     data_pca = pca.fit_transform(data_standardized)
     df = pd.DataFrame(data=data_pca, columns=['pc1', 'pc2'])
     df["labels"] = labels
     df["clusters"] = clusters
-    pca_plot = sns.scatterplot(data = df, x="pc1", y="pc2", hue="clusters", legend = False, palette="hls")
-    for i in range(0,n_labels):
+    pca_plot = sns.scatterplot(data = df, x="pc1", y="pc2", hue="clusters", legend = False, palette="hls", size=1)
+    for i in range(0,n_labels, LABEL_DENSITY):
         pca_plot.text(df.pc1[i]+0.01, df.pc2[i], 
         labels[i], horizontalalignment='left', 
-        size='small', color='black')
-    plt.savefig(f"{file_label}-pca.pdf")
+        size=6, color='black')
+    #plt.savefig(f"{file_label}-pca.pdf")
     #os.path.join(output_dir, f"{variable_param}-end-sb.{IMG_FORMAT}")
