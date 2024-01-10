c7ad12428747188c6401f9a6ad42d2f1f94c36cf
Peter Dekker <pdekker@ai.vub.ac.be>
2024-01-10 17:42:50 +0100

    Created data path structure for different languages. Removed archive from git

--- data.py
+++ data.py
@@ -11,48 +11,63 @@ import pandas as pd
 
 from conf import INFLECTION_CLASSES
 
-#np.random.seed(11)
+# np.random.seed(11)
 currentdir = os.path.abspath("")
 DATA_PATH = os.path.join(currentdir, "data")
 
-ROMANCE_ARCHIVE_PATH = "Romance_Verbal_Inflection_Dataset-v2.0.4.tar.gz"
-ROMANCE_ARCHIVE_URL = "https://gitlab.com/sbeniamine/Romance_Verbal_Inflection_Dataset/-/archive/v2.0.4/Romance_Verbal_Inflection_Dataset-v2.0.4.tar.gz"
-# Directory after unpacking archive
-ROMANCE_PATH = os.path.join(DATA_PATH, "Romance_Verbal_Inflection_Dataset-v2.0.4")
-ROMANCE_METADATA_PATH = os.path.join(ROMANCE_PATH, "cldf/Wordlist-metadata.json")
+# ROMANCE_ARCHIVE_PATH = "Romance_Verbal_Inflection_Dataset-v2.0.4.tar.gz"
+# ROMANCE_ARCHIVE_URL = "https://zenodo.org/records/4039059/files/v2.0.4.zip"
+# # Directory after unpacking archive
+# ROMANCE_FILE_PATH = os.path.join(
+#     DATA_PATH, "Romance_Verbal_Inflection_Dataset-v2.0.4")
+# ROMANCE_METADATA_PATH = os.path.join(
+#     ROMANCE_PATH, "cldf/Wordlist-metadata.json")
 
-PORTUGUESE_ARCHIVE_PATH = "https://zenodo.org/records/8392722/files/v2.0.1.zip"
-ESTONIAN_ARCHIVE_PATH = "https://zenodo.org/records/8392744/files/v1.0.1.zip"
-ARABIC_ARCHIVE_PATH = "https://zenodo.org/records/10100678/files/aravelex-1.0.zip"
+# PORTUGUESE_ARCHIVE_URL = "https://zenodo.org/records/8392722/files/v2.0.1.zip"
+# ESTONIAN_ARCHIVE_URL = "https://zenodo.org/records/8392744/files/v1.0.1.zip"
+# ARABIC_ARCHIVE_URL = "https://zenodo.org/records/10100678/files/aravelex-1.0.zip"
 
+
+paths = {
+    "latin":
+    {"archive_url": "https://zenodo.org/records/4039059/files/v2.0.4.zip",
+     "archive_path": "Romance_Verbal_Inflection_Dataset-v2.0.4.zip",
+     "file_path": os.path.join(DATA_PATH, "Romance_Verbal_Inflection_Dataset-v2.0.4"),
+     "metadata_relative_path": "cldf/Wordlist-metadata.json"},
+     "estonian":
+    {"archive_url": "https://zenodo.org/records/8392744/files/v1.0.1.zip"},
+    "portuguese":
+    {"archive_url": "https://zenodo.org/records/8392722/files/v2.0.1.zip"},
+    "arabic":
+    {"archive_url": "https://zenodo.org/records/10100678/files/aravelex-1.0.zip"},
+}
 ############ Methods Romance dataset ##########
 
 
-def download_if_needed(archive_path, archive_url, dataset_path, data_path, label):
-    if not os.path.exists(dataset_path):
+def download_if_needed(paths_lang, label):
+    if not os.path.exists(paths_lang["file_path"]):
         # Create parent dirs
-        #p = pathlib.Path(data_path)
-        #p.parent.mkdir(parents=True, exist_ok=True)
-        with open(archive_path, 'wb') as f:
-            print(f"Downloading {label} from {archive_url}")
+        # p = pathlib.Path(data_path)
+        # p.parent.mkdir(parents=True, exist_ok=True)
+        with open(paths_lang["archive_path"], 'wb') as f:
+            print(f'Downloading {label} from {paths_lang["archive_url"]}')
             try:
-                r = requests.get(archive_url, allow_redirects=True)
+                r = requests.get(paths_lang["archive_url"], allow_redirects=True)
             except requests.exceptions.RequestException as e:  # This is the correct syntax
                 raise SystemExit(e)
             # Write downloaded content to file
             f.write(r.content)
-            if not archive_path.endswith(".tar.gz"):
-                raise ValueError("Archive path does not end in .tar.gz.")
+            # if not paths_lang["archive_path"].endswith(".zip"):
+            #     raise ValueError("Archive path does not end in .zip.")
             print("Unpacking archive.")
-            os.makedirs(data_path, exist_ok=True)
-            shutil.unpack_archive(archive_path, extract_dir=data_path)
+            os.makedirs(paths_lang["file_path"], exist_ok=True)
+            shutil.unpack_archive(paths_lang["archive_path"], extract_dir=paths_lang["file_path"])
 
 
 def load_romance_dataset():
-    download_if_needed(ROMANCE_ARCHIVE_PATH, ROMANCE_ARCHIVE_URL,
-                       ROMANCE_PATH, DATA_PATH, "romance")
+    download_if_needed(paths["latin"], "Romance Verbal Inflection Dataset")
     print("Loading data...")
-    dataset = Dataset.from_metadata(ROMANCE_METADATA_PATH)
+    dataset = Dataset.from_metadata(os.path.join(paths["latin"]["file_path"], paths["latin"]["metadata_relative_path"]))
     forms_df = pd.DataFrame(dataset["FormTable"])
     cognates_df = pd.DataFrame(dataset["CognatesetTable"])
     lects_df = pd.DataFrame(dataset["LanguageTable"])
@@ -87,13 +102,13 @@ def get_sound_inventory(forms):
     max_form_len = max([len(x) for x in forms])
     return sound_inventory, max_form_len
 
-def get_existing_sound_Ngrams(forms, Ngrams): # NOT FINISHED!!
+def get_existing_sound_Ngrams(forms, Ngrams):  # NOT FINISHED!!
     sound_inventory = list(set(list("".join(forms))))
     Ngram_list=[]
     form_values=forms.values
 
-    for i1 in range(0,len(form_values)):
-        for i2 in range(0,len(form_values[i1])-(Ngrams-1)):
+    for i1 in range(0, len(form_values)):
+        for i2 in range(0, len(form_values[i1])-(Ngrams-1)):
             Ngram=form_values[i1][i2:i2+Ngrams]
             if Ngram not in Ngram_list:
                 Ngram_list.append(Ngram)
@@ -117,13 +132,13 @@ def create_onehot_forms_from_Ngrams(forms, Ngrams, empty_symbol=True, pool_verb_
 
     n_Ngrams = len(Ngram_list)
     # print(sounds)
-    #print(f"n_forms: {n_forms}")
-    #print(f"n_sounds: {n_sounds}")
-    #print(f"max_form_len: {max_form_len}")
+    # print(f"n_forms: {n_forms}")
+    # print(f"n_sounds: {n_sounds}")
+    # print(f"max_form_len: {max_form_len}")
     array = np.zeros(shape=(n_forms, n_Ngrams))
 
     for form_row, form in enumerate(forms):
-        for char_position in range(0,len(form)-(Ngrams-1)):
+        for char_position in range(0, len(form)-(Ngrams-1)):
             current_Ngram=form[char_position:char_position+Ngrams]
             index=Ngram_list.index(current_Ngram)
             array[form_row, index] = 1
@@ -138,9 +153,9 @@ def create_onehot_forms(forms, empty_symbol=True):
         sounds.append(".")
     n_sounds = len(sounds)
     # print(sounds)
-    #print(f"n_forms: {n_forms}")
-    #print(f"n_sounds: {n_sounds}")
-    #print(f"max_form_len: {max_form_len}")
+    # print(f"n_forms: {n_forms}")
+    # print(f"n_sounds: {n_sounds}")
+    # print(f"max_form_len: {max_form_len}")
     array = np.zeros(shape=(n_forms, n_sounds * max_form_len))
     for form_row, form in enumerate(forms):
         # print(form)
@@ -150,13 +165,13 @@ def create_onehot_forms(forms, empty_symbol=True):
                 char = form[char_position]
                 char_hot_index = sounds.index(char)
                 array[form_row, char_position*n_sounds+char_hot_index] = 1
-                #print(f"Char position {char_position} within form: {char}. Hot index: {char_hot_index}. Index: {form_row, char_position*n_sounds+char_hot_index}")
+                # print(f"Char position {char_position} within form: {char}. Hot index: {char_hot_index}. Index: {form_row, char_position*n_sounds+char_hot_index}")
             else:  # Char_position >= form_len; so word shorter than the longest word
                 if empty_symbol:
                     # Create symbol for these positions
                     empty_hot_index = sounds.index(".")
                     array[form_row, char_position*n_sounds+empty_hot_index] = 1
-                    #print(f"Char position {char_position} OUTSIDE form. Hot index: {empty_hot_index}. Index: {form_row, char_position*n_sounds+empty_hot_index}")
+                    # print(f"Char position {char_position} OUTSIDE form. Hot index: {empty_hot_index}. Index: {form_row, char_position*n_sounds+empty_hot_index}")
                 # Else: just leave the 0000s for empty symbol
     return array, sounds
 
@@ -180,7 +195,8 @@ def create_language_dataset(df, language, Ngrams=2, empty_symbol=True, language_
 
     if use_only_present:
         if use_only_3PL:
-            df_used=df_language[df_language['Cell'].str.contains("'PRS-IND', '3PL'")]
+            df_used=df_language[df_language['Cell'].str.contains(
+                "'PRS-IND', '3PL'")]
         else:
             df_used=df_language[df_language['Cell'].str.contains("'PRS-IND'")]
 
@@ -190,76 +206,100 @@ def create_language_dataset(df, language, Ngrams=2, empty_symbol=True, language_
     cogids = df_used[cogid_column]
     person_tags=df_used.Cell.str[-5:-2]
     unique_person_tags=person_tags.unique()
-    person_tags=person_tags.values #array(['1SG', '2SG', '3SG', ..., '1PL', '2PL', '3PL']
+    # array(['1SG', '2SG', '3SG', ..., '1PL', '2PL', '3PL']
+    person_tags=person_tags.values
     unique_verbs=cogids.unique()
 
-    forms_encoded, bigram_inventory = create_onehot_forms_from_Ngrams(forms, Ngrams, empty_symbol, concat_verb_features)
+    forms_encoded, bigram_inventory = create_onehot_forms_from_Ngrams(
+        forms, Ngrams, empty_symbol, concat_verb_features)
 
     if squeeze_into_verbs:
         if concat_verb_features:
-            #Make new pooled bigram inventory with person tags
+            # Make new pooled bigram inventory with person tags
             pooled_bigram_inventory=[]
             for p in range(len(unique_person_tags)):
-                pooled_bigram_inventory=np.append(pooled_bigram_inventory,([i + '_'+unique_person_tags[p] for i in bigram_inventory]),axis=0)
+                pooled_bigram_inventory=np.append(pooled_bigram_inventory, ([
+                                                  i + '_'+unique_person_tags[p] for i in bigram_inventory]), axis=0)
 
-            pooled_forms_encoded=np.empty((0,len(unique_person_tags)*forms_encoded.shape[1]))
+            pooled_forms_encoded=np.empty(
+                (0, len(unique_person_tags)*forms_encoded.shape[1]))
             pooled_inflections=[]
-            pool_order=unique_person_tags #array(['1SG', '2SG', '3SG', '1PL', '2PL', '3PL'],
-            for i in range(0,len(unique_verbs)):
-                
+            # array(['1SG', '2SG', '3SG', '1PL', '2PL', '3PL'],
+            pool_order=unique_person_tags
+            for i in range(0, len(unique_verbs)):
+
                 pooled_forms_encoded_for_verb=[]
-                indices_for_verb=np.where(cogids==unique_verbs[i])[0]
-                pooled_inflections.append(inflections.values[indices_for_verb[0]]) #Save inflection for this pooled verb, from the first position
-                person_tags_for_verb=person_tags[indices_for_verb] #Not necessarily in the same order as wanted
+                indices_for_verb =np.where(cogids == unique_verbs[i])[0]
+                # Save inflection for this pooled verb, from the first position
+                pooled_inflections.append(
+                    inflections.values[indices_for_verb[0]])
+                # Not necessarily in the same order as wanted
+                person_tags_for_verb=person_tags[indices_for_verb]
                 for p in range(len(pool_order)):
 
                     if pool_order[p] in person_tags_for_verb:
                         index=np.where(pool_order[p] == person_tags_for_verb)
                         index_in_forms_encoded=indices_for_verb[index[0][0]]
-                        pooled_forms_encoded_for_verb=np.append(pooled_forms_encoded_for_verb, forms_encoded[index_in_forms_encoded],axis=0)
+                        pooled_forms_encoded_for_verb=np.append(
+                            pooled_forms_encoded_for_verb, forms_encoded[index_in_forms_encoded], axis=0)
                     else:
-                        pooled_forms_encoded_for_verb=np.append(pooled_forms_encoded_for_verb, np.zeros((forms_encoded.shape[1])),axis=0)
-                
-                if set_common_features_to_zero:
-                    temp=np.reshape(pooled_forms_encoded_for_verb, (len(pool_order), forms_encoded.shape[1]))
-                    S=sum(temp)
-                    temp[:,S==6]=0 # If the same gram was activated in all person tenses, their sum here is 6
-                    pooled_forms_encoded_for_verb=np.reshape(temp, (len(pool_order)*forms_encoded.shape[1]))
+                        pooled_forms_encoded_for_verb=np.append(
+                            pooled_forms_encoded_for_verb, np.zeros((forms_encoded.shape[1])), axis=0)
 
-                pooled_forms_encoded=np.append(pooled_forms_encoded,pooled_forms_encoded_for_verb[None,:],axis=0)
+                if set_common_features_to_zero:
+                    temp=np.reshape(pooled_forms_encoded_for_verb,
+                                    (len(pool_order), forms_encoded.shape[1]))
+                    S=sum(temp)
+                    # If the same gram was activated in all person tenses, their sum here is 6
+                    temp[:, S == 6] =0
+                    pooled_forms_encoded_for_verb=np.reshape(
+                        temp, (len(pool_order)*forms_encoded.shape[1]))
+
+                pooled_forms_encoded=np.append(
+                    pooled_forms_encoded, pooled_forms_encoded_for_verb[None, :], axis=0)
             inflections = pooled_inflections
 
             forms_encoded=pooled_forms_encoded
             bigram_inventory=pooled_bigram_inventory
-        else:   #Squeeze all persons into a single bigram vector, without concatenating (One hot of existing bigrams over all person tenses)
-               #Make new pooled bigram inventory with person tags
-  
-            pooled_forms_encoded=np.empty((0,forms_encoded.shape[1]))
+        # Squeeze all persons into a single bigram vector, without concatenating (One hot of existing bigrams over all person tenses)
+        else:
+               # Make new pooled bigram inventory with person tags
+
+            pooled_forms_encoded=np.empty((0, forms_encoded.shape[1]))
             pooled_inflections=[]
-            pool_order=unique_person_tags #array(['1SG', '2SG', '3SG', '1PL', '2PL', '3PL'],
-            for i in range(0,len(unique_verbs)):
-                
-                pooled_forms_encoded_for_verb=np.zeros((1,len(bigram_inventory)))
-                indices_for_verb=np.where(cogids==unique_verbs[i])[0]
-                pooled_inflections.append(inflections.values[indices_for_verb[0]]) #Save inflection for this pooled verb, from the first position
-                person_tags_for_verb=person_tags[indices_for_verb] #Not necessarily in the same order as wanted
+            # array(['1SG', '2SG', '3SG', '1PL', '2PL', '3PL'],
+            pool_order=unique_person_tags
+            for i in range(0, len(unique_verbs)):
+
+                pooled_forms_encoded_for_verb=np.zeros(
+                    (1, len(bigram_inventory)))
+                indices_for_verb =np.where(cogids == unique_verbs[i])[0]
+                # Save inflection for this pooled verb, from the first position
+                pooled_inflections.append(
+                    inflections.values[indices_for_verb[0]])
+                # Not necessarily in the same order as wanted
+                person_tags_for_verb=person_tags[indices_for_verb]
                 for p in range(len(pool_order)):
 
                     if pool_order[p] in person_tags_for_verb:
                         index=np.where(pool_order[p] == person_tags_for_verb)
                         index_in_forms_encoded=indices_for_verb[index[0][0]]
-                        pooled_forms_encoded_for_verb=pooled_forms_encoded_for_verb+forms_encoded[index_in_forms_encoded]
-                
-                if set_common_features_to_zero:
-                    pooled_forms_encoded_for_verb[pooled_forms_encoded_for_verb==6]=0 # If the same gram was activated in all person tenses, their sum here is 6
+                        pooled_forms_encoded_for_verb=pooled_forms_encoded_for_verb + \
+                            forms_encoded[index_in_forms_encoded]
 
-                pooled_forms_encoded_for_verb=np.clip(pooled_forms_encoded_for_verb, 0, 1)
-                pooled_forms_encoded=np.append(pooled_forms_encoded,pooled_forms_encoded_for_verb,axis=0)
+                if set_common_features_to_zero:
+                    # If the same gram was activated in all person tenses, their sum here is 6
+                    pooled_forms_encoded_for_verb[pooled_forms_encoded_for_verb == 6] =0
+
+                pooled_forms_encoded_for_verb=np.clip(
+                    pooled_forms_encoded_for_verb, 0, 1)
+                pooled_forms_encoded=np.append(
+                    pooled_forms_encoded, pooled_forms_encoded_for_verb, axis=0)
             inflections = pooled_inflections
 
             forms_encoded=pooled_forms_encoded
 
-    
+
     inflections_onehot, inflection_inventory = create_onehot_inflections(
         inflections)
     return forms_encoded, inflections_onehot, list(forms), list(inflections), list(cogids), bigram_inventory
